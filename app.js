56a14ce0d77fe23b1a63436b5ef968599f60570a
Parth Dhingra <parth.21839@gmail.com>
2020-08-18 14:57:43 +0000

    architecture

--- app.js
+++ app.js
@@ -2,15 +2,14 @@ var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser");
 
+var homeRoutes = require("./routes/home");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 
 
-app.get("/" , function(req, res) {
-	res.render("home");
-})
+app.use(homeRoutes);
 
 app.listen(3000, function() { 
   console.log('BudhaCeo app has started on port 3000'); 
