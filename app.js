c3bd8af0df429ec7d2c0ddaa5b642e8b91ce79b2
Parth Dhingra <parth.21839@gmail.com>
2020-08-19 08:08:52 +0000

    edit navbar

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser");
 
-var homeRoutes = require("./routes/home");
+var homeRoutes  = require("./routes/home"),
+	eventRoutes = require("./routes/events");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -10,6 +11,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.use(homeRoutes);
+app.use(eventRoutes);
 
 app.listen(3000, function() { 
   console.log('BudhaCeo app has started on port 3000'); 
