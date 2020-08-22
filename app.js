3595a1ee91960a4549757083d9e5686100ef6e23
Parth Dhingra <parth.21839@gmail.com>
2020-08-22 14:29:26 +0000

    about page

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser");
 
-var homeRoutes  = require("./routes/home"),
+var homeRoutes    = require("./routes/home"),
 	programRoutes = require("./routes/programs"),
-    contactRoutes = require("./routes/contact");
+    contactRoutes = require("./routes/contact"),
+    aboutRoutes   = require("./routes/about");    
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -14,6 +15,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(homeRoutes);
 app.use(programRoutes);
 app.use(contactRoutes);
+app.use(aboutRoutes);
 
 app.listen(3000, function() { 
   console.log('BudhaCeo app has started on port 3000'); 
