95cd0c27f1c23cd83e51b2ea5d6eeb82fb5a7fa9
Parth Dhingra <parth.21839@gmail.com>
2020-08-22 18:27:16 +0000

    ..

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ var express        = require("express"),
     app            = express(),
     bodyParser     = require("body-parser");
 
-var homeRoutes    = require("./routes/home"),
-	programRoutes = require("./routes/programs"),
-    contactRoutes = require("./routes/contact"),
-    aboutRoutes   = require("./routes/about");    
+var homeRoutes        = require("./routes/home"),
+	programRoutes     = require("./routes/programs"),
+    contactRoutes     = require("./routes/contact"),
+    aboutRoutes       = require("./routes/about"),
+    corporateRoutes   = require("./routes/corporate"),
+    donateRoutes      = require("./routes/donate"); 
+
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -16,6 +19,8 @@ app.use(homeRoutes);
 app.use(programRoutes);
 app.use(contactRoutes);
 app.use(aboutRoutes);
+app.use(corporateRoutes);
+app.use(donateRoutes);
 
 app.listen(3000, function() { 
   console.log('BudhaCeo app has started on port 3000'); 
