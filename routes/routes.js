f4f17e80ecea540c03b8fc26cebf90b9552aed77
Ramakrishna <rapulam1@in.ibm.com>
2020-10-12 19:13:57 +0530

    updating home events and enabling the resources tab

--- routes/routes.js
+++ routes/routes.js
@@ -17,6 +17,10 @@ router.get("/donations" , function(req, res) {
 	res.render("donate");
 })
 
+router.get("/resources" , function(req, res) {
+	res.redirect("https://www.blog.buddhaceo.org/");
+})
+
 router.get("/corporateprograms" , function(req, res) {
 	res.render("corporate");
 })
