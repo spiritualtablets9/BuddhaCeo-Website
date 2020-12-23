37c0dd9229dc0ee3ab55cde561ce5895517cdbd5
spiritualtablets9 <50904029+spiritualtablets9@users.noreply.github.com>
2020-12-23 17:39:27 +0530

    udpating mongoose connection

--- app.js
+++ app.js
@@ -7,8 +7,18 @@ var routes        = require("./routes/routes"),
 
 //                 API WITH MONGODB BELOW
 api      = require("./routes/api");
-var mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://cluster0.cmgg5.mongodb.net/ceo?retryWrites=true&w=majority', { useNewUrlParser: true });
+// var mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://cluster0.cmgg5.mongodb.net/ceo?retryWrites=true&w=majority', { useNewUrlParser: true });
+const mongoose = require('mongoose');
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.connect('mongodb+srv://cluster0.cmgg5.mongodb.net/ceo?retryWrites=true&w=majority')
+    .then(() => console.log('Connect to MongoDB..'))
+    .catch(err => console.error('Could not connect to MongoDB..', err))
 var db = mongoose.connection;
 
 app.use(bodyParser.urlencoded({extended: true}));
