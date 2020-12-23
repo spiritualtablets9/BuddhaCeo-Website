1a632052f48f9a20bb8321c788fe02e29a74d35a
spiritualtablets9 <50904029+spiritualtablets9@users.noreply.github.com>
2020-12-23 17:51:50 +0530

    updating ganga event
    updating ganga event

--- routes/routes.js
+++ routes/routes.js
@@ -36,25 +36,20 @@ router.get("/meditation40d" , function(req, res) {
 router.get("/programs" , function(req, res) {
 	res.redirect("https://www.events.buddhaceo.org/");
 })
-router.get("/kailash" , function(req, res) {
-	res.redirect("https://www.events.buddhaceo.org/event-details/6-weeks-nov");
-})
-
-router.get("/olympus" , function(req, res) {
-	res.redirect("https://www.events.buddhaceo.org/event-details/olympus-a-6-weeks-guided-meditation-program-1");
-})
 
 router.get("/meru" , function(req, res) {
 	res.redirect("https://www.events.buddhaceo.org/event-details/master-your-mind-season-2-a-guided-meditation-for-confidence-manifestation-3-weeks");
+})
+
+router.get("/ganga" , function(req, res) {
+	res.redirect("https://www.events.buddhaceo.org/event-details/ganga-a-6-weeks-guided-meditation-online-program");
 })
 
 router.get("/Atlas" , function(req, res) {
 	res.redirect("https://www.events.buddhaceo.org/event-details/master-your-mind-season-3-a-guided-meditation-for-confidence-manifestation-3-weeks");
 })
 
-router.get("/ganga" , function(req, res) {
-	res.redirect("https://www.events.buddhaceo.org/event-details/ganga-a-6-weeks-guided-meditation-online-program");
-})
+
 
 
 module.exports = router;
