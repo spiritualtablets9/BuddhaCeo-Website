beda96938ce06e9c0631ff4c16811bf5d52fd1c4
spiritualtablets9 <50904029+spiritualtablets9@users.noreply.github.com>
2021-03-01 22:22:14 +0530

    short Url for godavari added

--- routes/routes.js
+++ routes/routes.js
@@ -59,4 +59,8 @@ router.get("/indus" , function(req, res) {
 router.get("/ganga" , function(req, res) {
 	res.redirect("https://www.events.buddhaceo.org/event-details/ganga-a-6-weeks-guided-meditation-online-program-2");
 })
+
+router.get("/godavari" , function(req, res) {
+	res.redirect("https://www.events.buddhaceo.org/event-details/godavari-akhanda-dhyana-3-week-intensive-guided-meditation");
+})
 module.exports = router;
